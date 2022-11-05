ce7a4962d57eb50540eaa79772b40a5237676bb6
unknown <is_metidji@esi.dz>
2022-11-05 11:10:24 +0100

    done with events

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // <p class="pargraph"> I'am pargraph</p>
+// function to add elmeent
 function addPargraphInTheEndWithContent(content){
     let newP = document.createElement("p") // <p></p>
     newP.innerText = content
@@ -23,9 +24,51 @@ function addPargraphInTheEnd(){
 }
 
 
+//make the "add paragrph" button add the content
 document.querySelector("#addParagraphButton").addEventListener("click" , function(){
     let valueToAdd= document.querySelector("#WhatToAdd").value
     addPargraphInTheEndWithContent(valueToAdd)
     document.querySelector("#WhatToAdd").value = ""
+})
+
+// the "add pargrpah" button makes the body red when the "mouseover"
+document.querySelector("#addParagraphButton").addEventListener("mouseover" , function (){
+    // document.querySelector("body").style.backgroundColor="red"
+    console.log("mouse over Event")
+    let body = document.querySelector("body")
+    body.style.backgroundColor="red"
 
 })
+
+// the "add pargrpah" button return back the body to white color
+document.querySelector("#addParagraphButton").addEventListener("mouseout" , function (){
+    // document.querySelector("body").style.backgroundColor="white".
+    console.log("mouse out Event")
+
+    // let body = document.querySelector("body")
+    // body.style.backgroundColor="white"
+
+    let body = document.querySelector("body")
+    body.removeAttribute("style")
+
+})
+
+// make every button with ".clickedBtnGreen" becomes green when we click him
+document.querySelectorAll(".clickedBtnGreen").forEach(function(btn){
+    btn.addEventListener("click" ,  makeGreen)
+})
+
+
+//  when i click the button "#disableGreenBtn" , the buttons will not be green anymore
+document.querySelector("#disableGreenBtn").addEventListener("click" , function (event){
+
+    document.querySelectorAll(".clickedBtnGreen").forEach((elem)=>{
+        elem.removeEventListener("click" , makeGreen)
+    })
+})
+
+
+function makeGreen(event){
+        console.log("event =", event.target)
+        event.target.style.backgroundColor="green"
+}
